1a3f423ffe2f7bdb816da49df855cc7cfcdee19b
Daan Zeeuwe <daan_zeeuwe@hotmail.com>
2017-06-14 11:17:03 +0200

    Added covariance from gps to utm point stamped message, by creating a new message type.

--- scripts/utm_adapter.py
+++ scripts/utm_adapter.py
@@ -5,6 +5,7 @@ import utm
 
 from sensor_msgs.msg import NavSatFix, NavSatStatus
 from geometry_msgs.msg import Point, PointStamped
+from nautonomous_gps_adapter.msg import PointStampedWithCovariance
 
 utm_publisher = 0
 
@@ -23,7 +24,7 @@ def gpsCallback(data):
     utm_easting, utm_northing = utm.from_latlon(latitude, longitude)[:2]
     point = Point(utm_easting, utm_northing, 0)
 
-    utm_publisher.publish(data.header, point)
+    utm_publisher.publish(data.header, point, data.position_covariance, data.position_covariance_type)
 
 if __name__ == '__main__':
      # In ROS, nodes are uniquely named. If two nodes with the same
@@ -33,7 +34,7 @@ if __name__ == '__main__':
     # run simultaneously.
     rospy.init_node('utm_adapter', anonymous=True)
 
-    utm_publisher = rospy.Publisher('/utm/fix', PointStamped, queue_size=10)
+    utm_publisher = rospy.Publisher('/utm/fix', PointStampedWithCovariance, queue_size=10)
 
     rospy.Subscriber("/gps/fix", NavSatFix, gpsCallback)
 
